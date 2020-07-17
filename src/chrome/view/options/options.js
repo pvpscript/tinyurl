a5bb7aea0b5c53de1800be39ba8a17fc7da6c874
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2020-07-16 21:52:46 -0300

    Settings now change storaged settings

--- src/chrome/view/options/options.js
+++ src/chrome/view/options/options.js
@@ -1,1 +1,51 @@
 window.location.hash = "#how-to-use";
+
+const settings = document.getElementById("settings");
+settings.addEventListener("change", (e) => {
+	chrome.storage.sync.get(['settings'], (result) => {
+		const element = e.target;
+		const settings = result.settings;
+
+		if (element.id === "enable-alerts") {
+			settings.alerts = element.checked;
+		} else if (element.id === "autocopy") {
+			settings.autoCopy = element.checked;
+		} else {
+			switch(element.id) {
+				case "ext-alert":
+					settings.alertType = "extension";
+					break;
+				case "page-alert":
+					settings.alertType = "page";
+					break;
+			}
+		}
+
+		chrome.storage.sync.set({settings: settings});
+	});
+});
+
+chrome.storage.sync.get(['settings'], (result) => {
+	const settings = result.settings;
+
+	const enableAlerts = document.getElementById("enable-alerts");
+	const autocopy = document.getElementById("autocopy");
+	const extAlert = document.getElementById("ext-alert");
+	const pageAlert = document.getElementById("page-alert");
+
+	console.log(settings);
+
+	enableAlerts.checked = settings.alerts;
+	autocopy.checked = settings.autoCopy;
+
+	switch(settings.alertType) {
+		case 'extension':
+			extAlert.checked = true;
+			break;
+		case 'page':
+			pageAlert.checked = true;
+			break;
+	}
+});
+
+
